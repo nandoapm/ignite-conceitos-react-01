eea05657968287e9bb1d852a39799ee5bad1e902
Fernando Melo <fernando@neomode.com.br>
2021-04-02 22:24:39 -0300

    ignite-conceitos-react-01 finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,36 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
-  function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+  const handleCreateNewTask = () => {
+    console.log('newTaskTitle')
+    if(!newTaskTitle) return;
+
+    const newTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+
+    //setTasks(oldState => [...oldState, newTask])
+    setTasks([...tasks, newTask])
+    setNewTaskTitle("")
+    console.log(tasks)
   }
 
-  function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+  const handleToggleTaskCompletion = (id: number) => {
+    const checkedTask = tasks.map(task => task.id === id ? {
+      ...tasks,
+      id: task.id,
+      title: task.title,
+      isComplete: !task.isComplete
+    } : task)
+
+    setTasks(checkedTask)
   }
 
-  function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+  const handleRemoveTask = (id: number) => {
+    const deleteTask = tasks.filter(task => task.id !== id)
+    setTasks(deleteTask)
   }
 
   return (
